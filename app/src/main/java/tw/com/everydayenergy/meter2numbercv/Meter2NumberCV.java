d2beac5851ce414833088e0af33f1be8e9abc986
ctang003 <ctang003@gmail.com>
2017-02-05 01:16:08 +0800

    A test app for OpenCV Camera 可以顯示 但是還是用現成的好了, 明天來改, opencv 的攝影機怪怪的

--- app/src/main/java/tw/com/everydayenergy/meter2numbercv/Meter2NumberCV.java
+++ app/src/main/java/tw/com/everydayenergy/meter2numbercv/Meter2NumberCV.java
@@ -78,17 +78,17 @@ public class Meter2NumberCV extends AppCompatActivity implements CvCameraViewLis
 
         mOpenCvCameraView.setCvCameraViewListener(this);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        //FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
 
     }
 
